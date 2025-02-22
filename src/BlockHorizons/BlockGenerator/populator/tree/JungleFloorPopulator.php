f64ee9839dbde04f4a64e813be589fbfddeeae4f
NuhDev <92098247+NuhDevX@users.noreply.github.com>
2025-02-22 15:38:22 +0700

    Update JungleFloorPopulator.php

--- src/BlockHorizons/BlockGenerator/populator/tree/JungleFloorPopulator.php
+++ src/BlockHorizons/BlockGenerator/populator/tree/JungleFloorPopulator.php
@@ -3,18 +3,17 @@
 namespace BlockHorizons\BlockGenerator\populator\tree;
 
 use BlockHorizons\BlockGenerator\object\NewJungleTree;
-use pocketmine\level\ChunkManager;
+use pocketmine\world\ChunkManager;
 use pocketmine\math\Vector3;
 use pocketmine\utils\Random;
-
 
 class JungleFloorPopulator extends JungleTreePopulator
 {
 
-    public function populateCount(ChunkManager $level, int $chunkX, int $chunkZ, Random $random): void
+    public function populateCount(ChunkManager $world, int $chunkX, int $chunkZ, Random $random): void
     {
-        $this->level = $level;
-        $chunk = $level->getChunk($chunkX, $chunkZ);
+        $this->world = $world;
+        $chunk = $world->getChunk($chunkX, $chunkZ);
         $amount = $random->nextBoundedInt($this->randomAmount + 1) + $this->baseAmount;
         $v = new Vector3();
 
@@ -25,7 +24,7 @@ class JungleFloorPopulator extends JungleTreePopulator
             if ($y === -1) {
                 continue;
             }
-            (new NewJungleTree(4 + $random->nextBoundedInt(7), 3))->generate($level, $random, $v->setComponents($x, $y, $z));
+            (new NewJungleTree(4 + $random->nextBoundedInt(7), 3))->generate($world, $random, $v->setComponents($x, $y, $z));
         }
     }
 
