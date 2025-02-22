c84a3b69e4591915fbf661ae7a7ac2f82208a496
NuhDev <92098247+NuhDevX@users.noreply.github.com>
2025-02-22 13:00:50 +0700

    Update WaterIcePopulator.php

--- src/BlockHorizons/BlockGenerator/populator/WaterIcePopulator.php
+++ src/BlockHorizons/BlockGenerator/populator/WaterIcePopulator.php
@@ -2,22 +2,22 @@
 namespace BlockHorizons\BlockGenerator\populator;
 
 use BlockHorizons\BlockGenerator\biomes\CustomBiome;
-use pocketmine\block\Block;
-use pocketmine\level\ChunkManager;
-use pocketmine\level\generator\populator\Populator;
+use pocketmine\block\BlockTypeIds;
+use pocketmine\world\ChunkManager;
+use pocketmine\world\generator\populator\Populator;
 use pocketmine\utils\Random;
 
 class WaterIcePopulator extends Populator {
 
-	public function populate(ChunkManager $level, int $chunkX, int $chunkZ, Random $random) : void {
-		$chunk = $level->getChunk($chunkX, $chunkZ);
+	public function populate(ChunkManager $world, int $chunkX, int $chunkZ, Random $random) : void {
+          $chunk = $world->getChunk($chunkX, $chunkZ);
         for ($x = 0; $x < 16; $x++) {
             for ($z = 0; $z < 16; $z++) {
                 $biome = CustomBiome::getBiome($chunk->getBiomeId($x, $z));
                 if ($biome->isFreezing()) {
                     $topBlock = $chunk->getHighestBlockAt($x, $z);
-                    if ($chunk->getBlockId($x, $topBlock, $z) == Block::STILL_WATER)     {
-                        $chunk->setBlockId($x, $topBlock, $z, Block::ICE);
+                    if ($chunk->getBlockStateId($x, $topBlock, $z) == BlockTypeIds::WATER)     {
+                        $chunk->setBlockStateId($x, $topBlock, $z, BlockTypeIds::ICE);
                     }
                 }
             }
