25fe6c3590d6447bd77594a422ffb11149a8aeb5
NuhDev <92098247+NuhDevX@users.noreply.github.com>
2025-02-22 15:26:31 +0700

    Update BedrockPopulator.php

--- src/BlockHorizons/BlockGenerator/populator/BedrockPopulator.php
+++ src/BlockHorizons/BlockGenerator/populator/BedrockPopulator.php
@@ -2,9 +2,9 @@
 
 namespace BlockHorizons\BlockGenerator\populator;
 
-use pocketmine\block\Block;
-use pocketmine\level\ChunkManager;
-use pocketmine\level\generator\populator\Populator;
+use pocketmine\block\BlockTypeIds;
+use pocketmine\world\ChunkManager;
+use pocketmine\world\generator\populator\Populator;
 use pocketmine\utils\Random;
 
 class BedrockPopulator extends Populator
@@ -16,15 +16,15 @@ class BedrockPopulator extends Populator
      * @param int $chunkZ
      * @param Random $random
      */
-    public function populate(ChunkManager $level, int $chunkX, int $chunkZ, Random $random): void
+    public function populate(ChunkManager $world, int $chunkX, int $chunkZ, Random $random): void
     {
-        $chunk = $level->getChunk($chunkX, $chunkZ);
+        $chunk = $world->getChunk($chunkX, $chunkZ);
         for ($x = 0; $x < 16; $x++) {
             for ($z = 0; $z < 16; $z++) {
-                $chunk->setBlockId($x, 0, $z, Block::BEDROCK);
+                $chunk->setBlockStateId($x, 0, $z, BlockTypeIds::BEDROCK);
                 for ($i = 1; $i < 5; $i++) {
                     if ($random->nextBoundedInt($i) == 0) { //decreasing amount
-                        $chunk->setBlockId($x, $i, $z, Block::BEDROCK);
+                        $chunk->setBlockStateId($x, $i, $z, BlockTypeIds::BEDROCK);
                     }
                 }
             }
