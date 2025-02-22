b6b958e3a07c9682a1ce7b1a64001a3a2a82f697
NuhDev <92098247+NuhDevX@users.noreply.github.com>
2025-02-22 15:59:18 +0700

    Update PopulatorHelpers.php

--- src/BlockHorizons/BlockGenerator/populator/helper/PopulatorHelpers.php
+++ src/BlockHorizons/BlockGenerator/populator/helper/PopulatorHelpers.php
@@ -2,18 +2,26 @@
 
 namespace BlockHorizons\BlockGenerator\populator\helper;
 
-use pocketmine\block\Block;
-use pocketmine\level\format\Chunk;
+use pocketmine\block\BlockTypeIds;
+use pocketmine\world\format\Chunk;
 
 class PopulatorHelpers
 {
 
     const NON_SOLID = [
-        Block::AIR => true,
-        Block::LEAVES => true,
-        Block::LEAVES2 => true,
-        Block::SNOW_LAYER => true,
-        Block::TALL_GRASS => true,
+        BlockTypeIds::AIR => true,
+        BlockTypeIds::OAK_LEAVES => true,
+        BlockTypeIds::JUNGLE_LEAVES => true,
+        BlockTypeIds::DARK_OAK_LEAVES => true,
+        BlockTypeIds::SPRUCE_LEAVES => true,
+        BlockTypeIds::BIRCH_LEAVES => true,
+        BlockTypeIds::MANGROVE_LEAVES => true,
+        BlockTypeIds::AZALEA_LEAVES => true,
+        BlockTypeIds::CHERRY_LEAVES => true,
+        BlockTypeIds::ACACIA_LEAVES => true,
+        BlockTypeIds::BIRCH_LEAVES => true,
+        BlockTypeIds::SNOW_LAYER => true,
+        BlockTypeIds::TALL_GRASS => true,
     ];
 
     private function __construct()
