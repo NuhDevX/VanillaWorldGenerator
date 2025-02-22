635d224e6098466a4f916169a72e8762ea1fb8b9
NuhDev <92098247+NuhDevX@users.noreply.github.com>
2025-02-22 14:48:16 +0700

    Update LilyPadPopulator.php

--- src/BlockHorizons/BlockGenerator/populator/LilyPadPopulator.php
+++ src/BlockHorizons/BlockGenerator/populator/LilyPadPopulator.php
@@ -4,7 +4,7 @@ namespace BlockHorizons\BlockGenerator\populator;
 
 use BlockHorizons\BlockGenerator\populator\helper\EnsureBelow;
 use BlockHorizons\BlockGenerator\populator\helper\EnsureCover;
-use pocketmine\block\Block;
+use pocketmine\block\BlockTypeIds;
 use pocketmine\level\format\Chunk;
 use pocketmine\utils\Random;
 
@@ -13,12 +13,12 @@ class LilyPadPopulator extends SurfaceBlockPopulator
 
     protected function canStay(int $x, int $y, int $z, Chunk $chunk): bool
     {
-        return EnsureCover::ensureCover($x, $y, $z, $chunk) && EnsureBelow::ensureBelow($x, $y, $z, Block::STILL_WATER, $chunk);
+        return EnsureCover::ensureCover($x, $y, $z, $chunk) && EnsureBelow::ensureBelow($x, $y, $z, BlockTypeIds::WATER, $chunk);
     }
 
     protected function getBlockId(int $x, int $z, Random $random, Chunk $chunk): int
     {
-        return Block::LILY_PAD;
+        return BlockTypeIds::LILY_PAD;
     }
 
 }
