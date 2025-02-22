4d079b421dcab9d987d830e89ecd9b23f321c5b3
NuhDev <92098247+NuhDevX@users.noreply.github.com>
2025-02-22 15:51:27 +0700

    Update EnsureGrassBelow.php

--- src/BlockHorizons/BlockGenerator/populator/helper/EnsureGrassBelow.php
+++ src/BlockHorizons/BlockGenerator/populator/helper/EnsureGrassBelow.php
@@ -2,8 +2,8 @@
 
 namespace BlockHorizons\BlockGenerator\populator\helper;
 
-use pocketmine\block\Block;
-use pocketmine\level\format\Chunk;
+use pocketmine\block\BlockTypeIds;
+use pocketmine\world\format\Chunk;
 
 class EnsureGrassBelow
 {
@@ -14,7 +14,7 @@ class EnsureGrassBelow
 
     public static function ensureGrassBelow(int $x, int $y, int $z, Chunk $chunk)
     {
-        return EnsureBelow::ensureBelow($x, $y, $z, Block::GRASS, $chunk);
+        return EnsureBelow::ensureBelow($x, $y, $z, BlockTypeIds::GRASS, $chunk);
     }
 
 }
