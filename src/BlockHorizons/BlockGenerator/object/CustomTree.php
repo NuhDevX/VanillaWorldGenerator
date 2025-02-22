303cc18df10f5212e8daf9e1aed94c3c17e13e6c
NuhDev <92098247+NuhDevX@users.noreply.github.com>
2025-02-22 17:57:41 +0700

    Update CustomTree.php

--- src/BlockHorizons/BlockGenerator/object/CustomTree.php
+++ src/BlockHorizons/BlockGenerator/object/CustomTree.php
@@ -3,14 +3,14 @@
 namespace BlockHorizons\BlockGenerator\object;
 
 use pocketmine\block\Block;
-use pocketmine\level\generator\object\Tree;
+use pocketmine\world\generator\object\Tree;
 
 abstract class CustomTree extends Tree
 {
 
     public function canOverride(Block $block): bool
     {
-        return isset($this->overridable[$block->getId()]);
+        return isset($this->overridable[$block->getTypeId()]);
     }
 
 }
