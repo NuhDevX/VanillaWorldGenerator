06b863afd05c5c377b2155a15aa9bf332baccf58
NuhDev <92098247+NuhDevX@users.noreply.github.com>
2025-02-22 15:08:10 +0700

    Update GrassPopulator.php

--- src/BlockHorizons/BlockGenerator/populator/GrassPopulator.php
+++ src/BlockHorizons/BlockGenerator/populator/GrassPopulator.php
@@ -3,8 +3,8 @@
 namespace BlockHorizons\BlockGenerator\populator;
 
 use BlockHorizons\BlockGenerator\populator\helper\PopulatorHelpers;
-use pocketmine\block\Block;
-use pocketmine\level\format\Chunk;
+use pocketmine\block\BlockTypeIds;
+use pocketmine\world\format\Chunk;
 use pocketmine\utils\Random;
 
 class GrassPopulator extends SurfaceBlockPopulator
@@ -17,7 +17,7 @@ class GrassPopulator extends SurfaceBlockPopulator
 
     protected function getBlockId(int $x, int $z, Random $random, Chunk $chunk): int
     {
-        return Block::TALL_GRASS;
+        return BlockTypeIds::TALL_GRASS;
     }
 
 }
