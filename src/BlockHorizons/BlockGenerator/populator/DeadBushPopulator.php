43d0f0421efdb25514b7570815f4a1db4a311f67
NuhDev <92098247+NuhDevX@users.noreply.github.com>
2025-02-22 15:23:47 +0700

    Update DeadBushPopulator.php

--- src/BlockHorizons/BlockGenerator/populator/DeadBushPopulator.php
+++ src/BlockHorizons/BlockGenerator/populator/DeadBushPopulator.php
@@ -4,17 +4,17 @@ namespace BlockHorizons\BlockGenerator\populator;
 use BlockHorizons\BlockGenerator\populator\helper\EnsureBelow;
 use BlockHorizons\BlockGenerator\populator\helper\EnsureCover;
 use pocketmine\block\Block;
-use pocketmine\level\format\Chunk;
+use pocketmine\world\format\Chunk;
 use pocketmine\utils\Random;
 
 class DeadBushPopulator extends SurfaceBlockPopulator {
 
     protected function canStay(int $x, int $y, int $z, Chunk $chunk) : bool {
-        return EnsureCover::ensureCover($x, $y, $z, $chunk) && EnsureBelow::ensureBelow($x, $y, $z, Block::SAND, $chunk);
+        return EnsureCover::ensureCover($x, $y, $z, $chunk) && EnsureBelow::ensureBelow($x, $y, $z, BlockTypeIds::SAND, $chunk);
     }
 
     protected function getBlockId(int $x, int $z, Random $random, Chunk $chunk) : int {
-        return Block::DEAD_BUSH;
+        return BlockTypeIds::DEAD_BUSH;
     }
 
 }
